83436424ba079380f2412704cf481fbe92009027
Luis Angel <luisangeldev@gmail.com>
2021-05-22 14:32:06 -0500

    Adds users index table

--- app/Http/Livewire/Admin/Users/UsersIndex.php
+++ app/Http/Livewire/Admin/Users/UsersIndex.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire\Admin\Users;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersIndex extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        return view('livewire.admin.users.users-index')->extends('layouts.app');;
+        $users = User::paginate(10);
+        return view('livewire.admin.users.users-index', [
+            'users' => $users
+        ])->extends('layouts.app');;
     }
 }
--- resources/views/livewire/admin/users/users-index.blade.php
+++ resources/views/livewire/admin/users/users-index.blade.php
@@ -1,3 +1,50 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="card">
+        <div class="card-header">
+            <strong>
+                Users
+            </strong>
+        </div>
+        <div class="card-body">
+            <table class="table table-responsive-sm table-bordered table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Verified at</th>
+                        <th>Created at</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->email_verified_at }}</td>
+                            <td>{{ $user->created_at }}</td>
+                            <td>
+                                <a href="{{ route('admin.users.detail', $user->id) }}"
+                                    class="btn btn-info btn-sm"
+                                >
+                                    View
+                                </a>
+                                <a href="{{ $user->id }}" class="btn btn-primary btn-sm">
+                                    Edit
+                                </a>
+                                <button class="btn btn-danger btn-sm">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {{ $users->links() }}
+        </div>
+    </div>
 </div>
